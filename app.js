c74159a26b759626ea56ae45c52a6c1dd947b3d8
PJ <jaminski@o2.pl>
2022-07-04 12:30:33 +0200

    form responsiveness

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const container = document.querySelector(".container");
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
 
 //creating a form switch
 sign_up_btn.addEventListener("click", ()=> {
@@ -8,4 +10,12 @@ sign_up_btn.addEventListener("click", ()=> {
 });
 sign_in_btn.addEventListener("click", ()=> {
     container.classList.remove("sign-up-mode");
+});
+
+//switch for small screen-Responsive
+sign_up_btn2.addEventListener("click", ()=> {
+    container.classList.add("sign-up-mode2");
+});
+sign_in_btn2.addEventListener("click", ()=> {
+    container.classList.remove("sign-up-mode2");
 });
